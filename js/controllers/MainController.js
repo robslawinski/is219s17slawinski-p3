ba4b6fa4458fe80b67a279b59caeac1d34d1c6ba
robslawinski <rslawinski@verizon.net>
2017-04-22 14:01:28 -0400

    File Cleanup - MainController.js, Github Button working

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,38 +1,9 @@
 app.controller('MainController', ['$scope',
 	function($scope){
     //$scope.title = 'Top Ten Swordsmen in Books';
-    $scope.promo = 'ayy';
 	$scope.title = 'IMDB + Robert Slawinski\'s Top 8 Movies';
 	$scope.owner = 'Robert Slawinski';
 	$scope.github = 'https://github.com/robslawinski/is219s17slawinski-p3';
-	
-	//$scope.title = 'Top Ten Swordsmen in Books';
-    $scope.promo = 'ayy';
-    $scope.products = 
-    [
-      {
-        name: 'The Book of Trees',
-        price: 19,
-        pubdate: new Date('2014', '03','08'),
-        cover: 'img/the-book-of-trees.jpg'
-    	},
-      {
-        name: 'Program or be Programmed',
-        price: 8,
-        pubdate: new Date('2013', '08','01'),
-        cover: 'img/program-or-be-programmed.jpg'
-      },
-      {
-        name: 'I Have No Mouth and I Must Scream',
-        price: 10,
-        pubdate: new Date('1967', '02','15')
-      },
-       {
-        name: 'Of Mice And Men',
-        price: 10,
-        pubdate: new Date('1937', '10','27')
-      },
-   ]
 	
 	/* THESE ARE YOUR LIST OF MOVIES AND ACCOMPANYING METADATA
 	//	
